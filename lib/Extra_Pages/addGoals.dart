95f20c71ff168434dd333f3894bbb1fef8e27abe
Yousef Al-khateeb <Soree_456@hotmail.com>
2022-09-24 18:49:46 +0300

    changes

--- lib/Extra_Pages/addGoals.dart
+++ lib/Extra_Pages/addGoals.dart
@@ -14,6 +14,7 @@ class _addGoalsState extends State<addGoals> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: Text('Add Goals'),
       ),
     );
--- lib/Pages/prizes.dart
+++ lib/Pages/prizes.dart
@@ -1,6 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+
+import '../Extra_Pages/RedeemPrize.dart';
+import '../backend_services/backend_services.dart';
+
+backend_services backendServices = backend_services();
 
 class prizes extends StatefulWidget {
   const prizes({Key? key}) : super(key: key);
@@ -13,7 +19,67 @@ class _prizesState extends State<prizes> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(),
+      body: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+          stream: backendServices.getPrizes(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            }
+            return SafeArea(
+              child: GestureDetector(
+                onTap: () => FocusScope.of(context).unfocus(),
+                child: ListView.builder(
+                  itemCount: snapshot.data!.size,
+                  itemBuilder: (context, index) => Column(
+                    children: [
+                      Text(
+                        'Your Points: ' +
+                            snapshot.data!.docs[index]
+                                .data()['points']
+                                .toString(),
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        'iTunes 50 dollar card',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Image.network(
+                          snapshot.data!.docs[index].data()['image_url'],
+                          height: 200.0),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        'Prize Points: ' +
+                            snapshot.data!.docs[index]
+                                .data()['points_needed']
+                                .toString(),
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      ElevatedButton(
+                          onPressed: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) => RedeemPrize()));
+                          },
+                          child: Text('Redeem'))
+                    ],
+                  ),
+                ),
+              ),
+            );
+          }),
     );
   }
 }
